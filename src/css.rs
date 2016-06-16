483b0c9439c227905ccd5f24909a4687dd06447c
Koji Ota <afterjnih@gmail.com>
2016-06-17 01:10:08 +0900

    complete part3

--- src/css.rs
+++ src/css.rs
@@ -1,43 +1,50 @@
-struct StyleSheet {
+use std::ascii::AsciiExt;
+
+pub struct StyleSheet {
     rules: Vec<Rule>,
 }
 
-struct Rule {
+pub struct Rule {
     selectors: Vec<Selector>,
     declarations: Vec<Declaration>,
 }
 
-enum Selector {
+pub enum Selector {
     Simple(SimpleSelector),
 }
 
-struct SimpleSelector {
+pub struct SimpleSelector {
     tag_name: Option<String>,
     id: Option<String>,
     class: Vec<String>,
 }
 
-struct Declaration {
+pub struct Declaration {
     name: String,
     value: Value,
 }
 
-enum Value {
+#[derive(Clone, PartialEq)]
+pub enum Value {
     Keyword(String),
     Length(f32, Unit),
     ColorValue(Color),
 }
 
-enum Unit {
+#[derive(Clone, PartialEq)]
+pub enum Unit {
     Px,
 }
 
-struct Color {
+#[derive(Clone, PartialEq, Default)]
+pub struct Color {
     r: u8,
     g: u8,
     b: u8,
     a: u8,
 }
+
+impl Copy for Color {}
 
 pub type Specificity = (usize, usize, usize);
 
@@ -51,7 +58,41 @@ impl Selector {
     }
 }
 
+impl Value {
+    pub fn to_px(&self) -> f32 {
+        match *self {
+            Value::Length(f, Unit::Px) => f,
+            _ => 0.0,
+        }
+    }
+}
+
+pub fn parse(source: String) -> StyleSheet {
+    let mut parser = Parser {
+        pos: 0,
+        input: source,
+    };
+    StyleSheet { rules: parser.parse_rules() }
+}
+
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
 impl Parser {
+    fn parse_rules(&mut self) -> Vec<Ref> {
+        let mut rules = Vec::new();
+        loop {
+            self.consume_whitespace();
+            if self.eof() {
+                break;
+            }
+            rules.push(self.parse_rule());
+        }
+        return rules;
+    }
+
     fn parse_rule(&mut self) -> Rule {
         Rule {
             selectors: parse_selectors(),
@@ -103,5 +144,116 @@ impl Parser {
             }
         }
         return selector;
+    }
+
+    fn parse_declarations(&mut self) -> Vec<Declaration> {
+        assert!(self.consume_char() == '{');
+        let mut declarations = Vec::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == '}' {
+                self.consume_char();
+                break;
+            }
+            declarations.push(self.parse_declaration());
+        }
+        return declarations;
+    }
+
+    fn parse_declaration(&mut self) -> Declaration {
+        let property_name = self.parse_identifier();
+        self.consume_whitespace();
+        assert!(self.consume_char() == ':');
+        self.consume_whitespace();
+        let value = self.parse_value();
+        self.consume_whitespace();
+        assert!(self.consume_char() == ';');
+
+        Declaration {
+            name: property_name,
+            value: value,
+        }
+    }
+
+    fn parse_value(&mut self) -> Value {
+        match self.next_char() {
+            '0'...'9' => self.parse_length(),
+            '#' => self.parse_color(),
+            _ => Value::Keyword(self.parse_identifier()),
+        }
+    }
+
+    fn parse_length(&mut self) -> Value {
+        Value::Length(self.parse_float(), self.parse_unit());
+    }
+
+    fn parse_float(&mut self) -> f32 {
+        let s = self.consume_while(|c| {
+            match c {
+                '0'...'9' | '.' => true,
+                _ => false,
+            }
+        });
+        s.parse().unwrap()
+    }
+
+    fn parse_unit(&mut self) -> Unit {
+        match &*self.parse_identifier().to_ascii_lowercase() {
+            "px" => Unit::Px,
+            _ => panic!("unrecognized unit"),
+        }
+    }
+
+    fn parse_color(&mut self) -> Value {
+        assert!(self.consume_char() == '#');
+        Value::ColorValue(Color {
+            r: self.parse_hex_pair(),
+            g: self.parse_hex_pair(),
+            b: self.parse_hex_pair(),
+            a: 255,
+        })
+    }
+
+    fn parse_hex_pair(&mut self) -> u8 {
+        let s = &self.input[self.pos..self.pos + 2];
+        self.pos = self.pos + 2;
+        u8::from_str_radix(s, 16).unwrap()
+    }
+
+    fn parse_identifier(&mut self) -> String {
+        self.consume_while(valid_identifier_char);
+    }
+
+    fn consume_while<F>(&mut self, test: F) -> String
+        where F: Fn(char) -> bool
+    {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        return result;
+    }
+
+    fn consume_char(&self) -> char {
+        let mut iter = self.input[self.pos..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.pos += next_pos;
+        return cur_char;
+    }
+
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+}
+
+fn valid_identifier_char(c: char) -> bool {
+    match c {
+        'a'...'z' | 'A'...'Z' | '0'...'9' | '-' | '_' => true,
+        _ => false,
     }
 }
