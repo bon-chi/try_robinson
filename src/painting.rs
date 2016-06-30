a784d971ddc18337e3ddc074b0955cc7ada657f4
Koji Ota <200246@MBP0100.local>
2016-06-30 13:10:27 +0900

    finish part7

--- src/painting.rs
+++ src/painting.rs
@@ -1,5 +1,13 @@
 type DisplayList = Vec<DisplayCommand>;
 
+fn paint(layout_root: &LayoutBox, bounds: Rect) -> Canvas {
+    let display_list = build_display_list(layout_root);
+    let mut canvas = Canvas::new(bounds.width as usize, bounds.height as usize);
+    for item in display_list {
+        canvs.paint_item(&item);
+    }
+    return canvas;
+}
 enum DisplayCommand {
     SolidColor(Color, Rect),
 }
@@ -74,3 +82,49 @@ fn render_borders(list: &mut DisplayList, layout_box: &LayoutBox) {
                                              height: d.border.bottom,
                                          }));
 }
+
+struct Canvas {
+    pixels: Vec<Color>,
+    width: usize,
+    height: usize,
+}
+
+impl Canvas {
+    fn new(width: usize, height: usize) -> Canvas {
+        let white = Color {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255,
+        };
+        return Canvas {
+            pixels: repeat(white).take(width * height).collect(),
+            width: width,
+            hegiht: height,
+        };
+    }
+
+    fn paint_items(&mut self, item: &DisplayCommand) {
+        match item {
+            &DisplayCommand::SolidColor(color, rect) => {
+                let x0 = rect.x.clamp(0.0, self.width as f32) as usize;
+                let y0 = rect.y.clamp(0.0, self.height as f32) as usize;
+                let x1 = (rect.x + rect.width).clamp(0.0, self.width as f32) as usize;
+                let y1 = (rect.y + rect.hegiht).clamp(0, 0, self.height as f32) as usize;
+                for y in (y0..y1) {
+                    for x in (x0..x1) {
+                        self.pixels[x + y * self.width] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
+trait FloatClamp : Float {
+    fn clamp(self, lower: Self, upper: Self) -> Self {
+        self.max(lower).min(upper)
+    }
+}
+
+impl<T: Folat> FloatClamp for T {}
